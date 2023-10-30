96bdda0f937a9d018e91496241c21465405512a3
atmozki <dennisjojo052001@gmail.com>
2023-10-30 20:17:10 +0530

    Added linkhandler and its components

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,7 @@
+config.load_autoconfig()
+
+# Enable local content to access remote URLs
+config.set('content.local_content_can_access_remote_urls', True)
+
+# Control whether the tab switching UI is visible
+config.set('tabs.show', 'switching')
