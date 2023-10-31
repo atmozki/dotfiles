43568908ec70d1ac1ce6f895eb11b80361ca4b75
atmozki <dennisjojo052001@gmail.com>
2023-10-31 18:29:40 +0530

    Added bspwmrice config

---
+++ .config/bspwm/autostart.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+## General ------------------------------------------------------------------------#
+
+## Bspwm config directory
+BSPDIR="$HOME/.config/bspwm"
+
+## Sxhkd config directory
+SXHDIR="$HOME/.config/sxhkd"
+
+## Export bspwm/bin dir to PATH
+export PATH="${PATH}:$HOME/.config/bspwm/bin"
+
+## Run java applications without issues
+export _JAVA_AWT_WM_NONREPARENTING=1
+
+## Configurations ------------------------------------------------------------------#
+
+## Manager Workspaces
+
+# Xrandr config (Set resolution)
+
+if [[ $(xrandr --query | grep 'primary') ]]; then
+   xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --off
+fi
+
+# Autostart-----------------------------------------------------------------------#
+
+# Kill if already running
+killall -9 sxhkd dunst polybar conky
+
+# polkit agent
+if [[ ! `pidof polkit-gnome` ]]; then
+	/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+fi
+
+# Lauch keybindings daemon
+sxhkd -c "$SXHDIR"/sxhkdrc &
+
+# Start network applet
+nm-applet &
+
+#Start Xfce powermanager
+xfce4-power-manager &
+
+# Fix cursor
+xsetroot -cursor_name left_ptr
+
+# Polybar autostart
+$HOME/.config/polybar/launch.sh
+
+# Picom autostart
+picom -b &
+
+# Start conky
+conky -c "$HOME"/.config/conky/bspwm/config &
+
+# Start Doom Emacs
+/usr/bin/emacs --daemon &
+
+# Start mpd
+# exec mpd &
+
+# Other applications
+# nitrogen --restore &
+# run caffeine &
+# run dropbox &
+# run discord &
+## Wallpapers ---------------------------------------------------------------#
+
+## Get the wallpaper
+feh --bg-fill --randomize ~/Pictures/Walls/Desktop/
